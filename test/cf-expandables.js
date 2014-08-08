f44883dadc65d57d2d28399c6575d60f7021631e
Mike Morici <Michael.Morici@cfpb.gov>
2014-08-07 22:14:32 -0400

    More formatting tweaks.

--- test/cf-expandables.js
+++ test/cf-expandables.js
@@ -1,4 +1,4 @@
-(function($) {
+(function( $ ) {
   /*
     ======== A Handy Little QUnit Reference ========
     http://api.qunitjs.com/
@@ -20,7 +20,7 @@
       throws(block, [expected], [message])
   */
 
-  module('cf-expandables', {
+  module( 'cf-expandables', {
     // This will run before each test in this module.
     setup: function() {
       this.$testSubjectOne = $('#test-subject-one');
@@ -28,8 +28,8 @@
     }
   });
 
-  test('Verify initial default collapsed state', function() {
-    expect(6);
+  test( 'Verify initial default collapsed state', function() {
+    expect( 6 );
     ok(
       !$('#test-subject-one .expandable_content').is(':visible'),
       'The content should be collapsed'
@@ -43,21 +43,21 @@
       'The close cue should be hidden'
     );
     ok(
-      ($('#test-subject-one .expandable_target').attr('aria-pressed') === 'false'),
+      ( $('#test-subject-one .expandable_target').attr('aria-pressed') === 'false' ),
       'The target should have an aria-pressed attribute set to false'
     );
     ok(
-      ($('#test-subject-one .expandable_target').attr('aria-controls') === $('#test-subject-one .expandable_content').attr('id')),
+      ( $('#test-subject-one .expandable_target').attr('aria-controls') === $('#test-subject-one .expandable_content').attr('id') ),
       'The target should have an aria-controls attribute set to the id attribute of the content'
     );
     ok(
-      ($('#test-subject-one .expandable_content').attr('aria-expanded') === 'false'),
+      ( $('#test-subject-one .expandable_content').attr('aria-expanded') === 'false' ),
       'The content should have an aria-expanded attribute set to false'
     );
   });
 
-  test('Verify initial state when using the expanded modifier', function() {
-    expect(6);
+  test( 'Verify initial state when using the expanded modifier', function() {
+    expect( 6 );
     ok(
       $('#test-subject-two .expandable_content').is(':visible'),
       'The content should be expanded'
@@ -71,21 +71,21 @@
       'The open cue should be hidden'
     );
     ok(
-      ($('#test-subject-two .expandable_target').attr('aria-pressed') === 'true'),
+      ( $('#test-subject-two .expandable_target').attr('aria-pressed') === 'true' ),
       'The target should have an aria-pressed attribute that is true'
     );
     ok(
-      ($('#test-subject-two .expandable_target').attr('aria-controls') === $('#test-subject-two .expandable_content').attr('id')),
+      ( $('#test-subject-two .expandable_target').attr('aria-controls') === $('#test-subject-two .expandable_content').attr('id') ),
       'The target should have an aria-controls attribute set to the id attribute of the content'
     );
     ok(
-      ($('#test-subject-two .expandable_content').attr('aria-expanded') === 'true'),
+      ( $('#test-subject-two .expandable_content').attr('aria-expanded') === 'true' ),
       'The content should have an aria-expanded attribute set to true'
     );
   });
 
-  asyncTest('Verify expandables can open after being closed by default', function() {
-    expect(5);
+  asyncTest( 'Verify expandables can open after being closed by default', function() {
+    expect( 5 );
     var $expandable = this.$testSubjectOne;
     $expandable.find('.expandable_target').trigger('click');
     setTimeout(function() {
@@ -102,19 +102,19 @@
         'The open cue should be hidden'
       );
       ok(
-        ($expandable.find('.expandable_target').attr('aria-pressed') === 'true'),
+        ( $expandable.find('.expandable_target').attr('aria-pressed') === 'true' ),
         'The target should have an aria-pressed attribute that is true'
       );
       ok(
-        ($expandable.find('.expandable_content').attr('aria-expanded') === 'true'),
+        ( $expandable.find('.expandable_content').attr('aria-expanded') === 'true' ),
         'The content should have an aria-expanded attribute set to true'
       );
       start();
     }, 900);
   });
 
-  asyncTest('Verify expandables can close after being opened by a click', function() {
-    expect(5);
+  asyncTest( 'Verify expandables can close after being opened by a click', function() {
+    expect( 5 );
     var $expandable = this.$testSubjectOne;
     // This expandable was opened in the previous test
     $expandable.find('.expandable_target').trigger('click');
@@ -132,19 +132,19 @@
         'The close cue should be hidden'
       );
       ok(
-        ($expandable.find('.expandable_target').attr('aria-pressed') === 'false'),
+        ( $expandable.find('.expandable_target').attr('aria-pressed') === 'false' ),
         'The target should have an aria-pressed attribute that is false'
       );
       ok(
-        ($expandable.find('.expandable_content').attr('aria-expanded') === 'false'),
+        ( $expandable.find('.expandable_content').attr('aria-expanded') === 'false' ),
         'The content should have an aria-expanded attribute set to true'
       );
       start();
     }, 1800);
   });
 
-  asyncTest('Verify expandables can close after being open by default', function() {
-    expect(5);
+  asyncTest( 'Verify expandables can close after being open by default', function() {
+    expect( 5 );
     var $expandable = this.$testSubjectTwo;
     $expandable.find('.expandable_target').trigger('click');
     setTimeout(function() {
@@ -161,19 +161,19 @@
         'The close cue should be hidden'
       );
       ok(
-        ($expandable.find('.expandable_target').attr('aria-pressed') === 'false'),
+        ( $expandable.find('.expandable_target').attr('aria-pressed') === 'false' ),
         'The target should have an aria-pressed attribute that is false'
       );
       ok(
-        ($expandable.find('.expandable_content').attr('aria-expanded') === 'false'),
+        ( $expandable.find('.expandable_content').attr('aria-expanded') === 'false' ),
         'The content should have an aria-expanded attribute set to true'
       );
       start();
     }, 900);
   });
 
-  asyncTest('Verify expandables can open after being closed by a click', function() {
-    expect(5);
+  asyncTest( 'Verify expandables can open after being closed by a click', function() {
+    expect( 5 );
     var $expandable = this.$testSubjectTwo;
     // This expandable was opened in the previous test
     $expandable.find('.expandable_target').trigger('click');
@@ -191,29 +191,29 @@
         'The open cue should be hidden'
       );
       ok(
-        ($expandable.find('.expandable_target').attr('aria-pressed') === 'true'),
+        ( $expandable.find('.expandable_target').attr('aria-pressed') === 'true' ),
         'The target should have an aria-pressed attribute that is true'
       );
       ok(
-        ($expandable.find('.expandable_content').attr('aria-expanded') === 'true'),
+        ( $expandable.find('.expandable_content').attr('aria-expanded') === 'true' ),
         'The content should have an aria-expanded attribute set to true'
       );
       start();
     }, 1800);
   });
 
-  test('Verify dynamic duration based on height', function() {
-    expect(2);
-    $('#test-subject-one .expandable_content').height(200);
+  test( 'Verify dynamic duration based on height', function() {
+    expect( 2 );
+    $('#test-subject-one .expandable_content').height( 200 );
     ok(
-      ($.fn.expandable.calculateExpandDuration($('#test-subject-one .expandable_content').height()) === 800),
+      ( $.fn.expandable.calculateExpandDuration($('#test-subject-one .expandable_content').height()) === 800 ),
       'The duration should be (height * 4) when expanding'
     );
-    $('#test-subject-two .expandable_content').height(200).trigger('click');
+    $('#test-subject-two .expandable_content').height( 200 ).trigger('click');
     ok(
-      ($.fn.expandable.calculateCollapseDuration($('#test-subject-one .expandable_content').height()) === 400),
+      ( $.fn.expandable.calculateCollapseDuration($('#test-subject-one .expandable_content').height()) === 400 ),
       'The duration should be (height * 2) when collapsing'
     );
   });
 
-}(jQuery));
+}( jQuery ));
